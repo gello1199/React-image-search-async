401ac185896a0ca06315c3d580d8b8dfa470123e
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-12-27 18:11:04 -0500

    desctructure alt and src tags

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 class ImageCard extends Component {
     render() {
+        const {description, urls} = this.props.image
+
         return (
             <div>
                 <img 
-                    alt={this.props.image.description}
-                    src={this.props.image.urls.regular}
+                    alt={description}
+                    src={urls.regular}
                  />
             </div>
         );
