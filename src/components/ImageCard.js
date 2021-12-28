cfbc5434132c14a842b4265844a1a1d2799e74c4
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-12-27 19:48:21 -0500

    Add event listener and callback to mount

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 
 class ImageCard extends Component {
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight)
+    }
+
     render() {
         const {description, urls} = this.props.image
 
         return (
             <div>
-                <img 
+                <img
+                    ref={this.imageRef} 
                     alt={description}
                     src={urls.regular}
                  />
